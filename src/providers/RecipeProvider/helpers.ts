a4ed2635fb669fff35e3a402936740dec7b14310
Brian West <brianwilliamwest@gmail.com>
2020-01-06 20:02:07 -0500

    refactor: recipe card

--- src/providers/RecipeProvider/helpers.ts
+++ src/providers/RecipeProvider/helpers.ts
@@ -1,6 +1,14 @@
 import { Recipe } from 'providers/RecipeProvider/types'
 
-const proteins = ['steak', 'beef', 'pork', 'chicken', 'turkey', 'tofu']
+export type Protein = 'steak' | 'beef' | 'pork' | 'chicken' | 'turkey' | 'tofu'
+const proteins: Protein[] = [
+  'steak',
+  'beef',
+  'pork',
+  'chicken',
+  'turkey',
+  'tofu',
+]
 
 export const determineProtein = (recipe: Recipe) => {
   return proteins.find(protein => {
